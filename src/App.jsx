b6185fa4aadfc3c0032039410f86b2b19ed00691
Dallin Whiting <dallinkwhiting@gmail.com>
2023-08-02 02:03:57 -0600

    Added login page with register/login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 
 import './App.css'
+import LoginForm from './LoginForm'
 
 function App() {
   const [count, setCount] = useState(0)
 
+ 
+
   return (
-    <>
-      
-    </>
+    <div className="bg-slate-200 flex flex-col w-full h-full items-center justify-center">
+      <LoginForm />
+    </div>
   )
 }
 
